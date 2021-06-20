74266b036ff0c4911e26008444d260f76d76ca92
luis-domingo <luisdomingo.18@gmail.com>
2021-06-20 19:16:53 +0200

    Done

--- src/app/components/pages/dashboard/contact/contact.component.ts
+++ src/app/components/pages/dashboard/contact/contact.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Contact } from 'src/app/shared/interfaces/contact';
-//import service
+import { ContactService } from 'src/app/shared/services/contact/contact.service';
 
 @Component({
   selector: 'app-contact',
@@ -9,10 +9,24 @@ import { Contact } from 'src/app/shared/interfaces/contact';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  contacts: Contact[] | undefined;
+  userLoggedIn: Boolean | undefined = (localStorage.getItem('isLogged') == 'true');
 
-  constructor() { }
+  constructor(private router: Router, private contactService: ContactService) { }
 
   ngOnInit(): void {
+    if(this.userLoggedIn == true){
+      this.contactService.getContacts().subscribe(
+        (res) => {
+          this.contacts = res;
+        }, 
+        (err) => {
+          this.router.navigateByUrl('/');
+        }
+      );
+    } else {
+      this.router.navigateByUrl('/');
+    }
   }
 
 }
